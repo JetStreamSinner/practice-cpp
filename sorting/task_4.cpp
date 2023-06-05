4114a02417da55b9ae5423cbb731489adca0bd3d
Script <bigalex934@gmail.com>
2023-06-05 15:03:29 +0300

    Implemented solution for sort task 4

--- sorting/task_4.cpp
+++ sorting/task_4.cpp
@@ -27,6 +27,7 @@ plus: try `stable_sort` and examine its guarantees
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include "gtest/gtest.h"
 
 /*
 enum class Rating {
@@ -45,14 +46,124 @@ struct Company {
   uint64_t capitalization;
 };*/
 
+bool compareImplementation(const Company& first, const Company& second, bool reverse)
+{
+    if (first.dividends != second.dividends) {
+        return reverse ? first.dividends < second.dividends : first.dividends > second.dividends;
+    }
+
+    if (first.rating != second.rating) {
+        return reverse ? first.rating < second.rating : first.rating > second.rating;
+    }
+
+    if (first.capitalization != second.capitalization) {
+        return reverse ? first.capitalization < second.capitalization : first.capitalization > second.capitalization;
+    }
+
+    return true;
+}
+
+struct Ascending {
+    bool operator()(const Company& first, const Company& second)
+    {
+        return compareImplementation(first, second, false);
+    }
+};
+
+struct Descending {
+    bool operator()(const Company& first, const Company& second)
+    {
+        return compareImplementation(first, second, true);
+    }
+};
+
+TEST(CompaniesSort, Ascending) {
+    std::vector<Company> input = {{"DailyMarket", {}, true, Rating::Hold, 15568997},
+                                  {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+                                  {"TeleBank", {}, false, Rating::Buy, 17549087},
+                                  {"FrontTravel", {}, true, Rating::Hold, 631681},
+                                  {"HiddenJets", {}, true, Rating::Outperform, 12008927}};
+
+    std::sort(input.begin(), input.end(), Ascending());
+
+    std::vector<Company> expected = {
+        {"HiddenJets", {}, true, Rating::Outperform, 12008927},
+        {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+        {"DailyMarket", {}, true, Rating::Hold, 15568997},
+        {"FrontTravel", {}, true, Rating::Hold, 631681},
+        {"TeleBank", {}, false, Rating::Buy, 17549087},
+    };
+
+    EXPECT_EQ(input, expected);
+    EXPECT_TRUE(std::is_sorted(expected.begin(), expected.end(), Ascending()));
+}
+
+TEST(CompaniesSort, Descending) {
+    std::vector<Company> input = {{"DailyMarket", {}, true, Rating::Hold, 15568997},
+                                  {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+                                  {"TeleBank", {}, false, Rating::Buy, 17549087},
+                                  {"FrontTravel", {}, true, Rating::Hold, 631681},
+                                  {"HiddenJets", {}, true, Rating::Outperform, 12008927}};
+
+    std::sort(input.begin(), input.end(), Descending());
+
+    std::vector<Company> expected = {
+        {"TeleBank", {}, false, Rating::Buy, 17549087},
+        {"FrontTravel", {}, true, Rating::Hold, 631681},
+        {"DailyMarket", {}, true, Rating::Hold, 15568997},
+        {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+        {"HiddenJets", {}, true, Rating::Outperform, 12008927},
+    };
+
+    EXPECT_EQ(input, expected);
+    EXPECT_TRUE(std::is_sorted(expected.begin(), expected.end(), Descending()));
+}
+
+TEST(CompaniesStableSort, Ascending)
+{
+    std::vector<Company> input = {{"DailyMarket", {}, true, Rating::Hold, 15568997},
+                                  {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+                                  {"TeleBank", {}, false, Rating::Buy, 17549087},
+                                  {"FrontTravel", {}, true, Rating::Hold, 631681},
+                                  {"HiddenJets", {}, true, Rating::Outperform, 12008927}};
+
+    std::stable_sort(input.begin(), input.end(), Ascending());
+
+    std::vector<Company> expected = {
+        {"HiddenJets", {}, true, Rating::Outperform, 12008927},
+        {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+        {"DailyMarket", {}, true, Rating::Hold, 15568997},
+        {"FrontTravel", {}, true, Rating::Hold, 631681},
+        {"TeleBank", {}, false, Rating::Buy, 17549087},
+    };
+
+    EXPECT_EQ(input, expected);
+    EXPECT_TRUE(std::is_sorted(expected.begin(), expected.end(), Ascending()));
+}
+
+TEST(CompaniesStableSort, Descending)
+{
+    std::vector<Company> input = {{"DailyMarket", {}, true, Rating::Hold, 15568997},
+                                  {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+                                  {"TeleBank", {}, false, Rating::Buy, 17549087},
+                                  {"FrontTravel", {}, true, Rating::Hold, 631681},
+                                  {"HiddenJets", {}, true, Rating::Outperform, 12008927}};
+
+    std::stable_sort(input.begin(), input.end(), Descending());
+
+    std::vector<Company> expected = {
+        {"TeleBank", {}, false, Rating::Buy, 17549087},
+        {"FrontTravel", {}, true, Rating::Hold, 631681},
+        {"DailyMarket", {}, true, Rating::Hold, 15568997},
+        {"SecurityWine", {}, true, Rating::Outperform, 8729992},
+        {"HiddenJets", {}, true, Rating::Outperform, 12008927},
+    };
+
+    EXPECT_EQ(input, expected);
+    EXPECT_TRUE(std::is_sorted(expected.begin(), expected.end(), Descending()));
+}
+
 int main() {
-  std::vector<Company> companies = generateRandomCompanies(18);
-
-  // ...
-
-  for (const auto& c : companies) {
-    std::cout << c << std::endl;
-  }
-
-  return 0;
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
